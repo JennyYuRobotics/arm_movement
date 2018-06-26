11f50517caec306210b0d32f745f8e3a0344d583
alexanderylau <39836625+alexanderylau@users.noreply.github.com>
2018-06-25 23:26:02 -0700

    Add files via upload

---
+++ Senior_Project_Vacuum_Pump.ino
@@ -0,0 +1,21 @@
+char serialData;
+int pin = 9;
+void setup() 
+{
+ pinMode(pin, OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+    serialData = Serial.read();    
+
+    if(serialData == '0')
+    {
+      digitalWrite(pin, HIGH);
+    }
+    else if(serialData == '1')
+    {
+      digitalWrite(pin, LOW);
+    }
+    Serial.println(serialData);
+}
